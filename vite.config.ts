96a2122cc64d2c25a89ba42c8e6dd13f5e7be938
pynoah <aklat2525123@gmail.com>
2025-05-29 23:16:42 +0900

    Save current progress

--- vite.config.ts
+++ vite.config.ts
@@ -30,9 +30,11 @@ export default defineConfig({
         display: 'standalone',
         background_color: '#ffffff',
         icons: [
-          { src: '/pwa-192x192.png', sizes: '192x192', type: 'image/png' },
-          { src: '/pwa-512x512.png', sizes: '512x512', type: 'image/png' }
+          { src: '/angry-todolist/pwa-192x192.png', sizes: '192x192', type: 'image/png' },
+          { src: '/angry-todolist/pwa-512x512.png', sizes: '512x512', type: 'image/png' }
         ]
+
+
       },
       workbox: {
         globPatterns: ['**/*.{js,css,html,png,svg,mp3}'],
