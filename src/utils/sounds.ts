d7458d85299525fc0a5a0499da031a5f218ac970
pynoah <aklat2525123@gmail.com>
2025-05-30 12:18:19 +0900

    作業中の変更を一時保存

--- src/utils/sounds.ts
+++ src/utils/sounds.ts
@@ -1,7 +1,7 @@
 const soundFiles = {
-  add: ['「はい」.mp3', '「はいは～い♪」.mp3', '「はいはいは～い！」.mp3'],
-  delete: ['「こら！」.mp3', '「なんだザコかあ」.mp3'],
-  complete: ['「頑張ったね」.mp3', '「すごいすごい」.mp3'],
+  add: ['hai.mp3', 'haihai.mp3', 'haihaihai.mp3'],
+  delete: ['hey.mp3', 'zako.mp3'],
+  complete: ['goodjob.mp3', 'goodgood.mp3'],
 }
 
 export function playRandomSound(type: 'add' | 'delete' | 'complete'): void {
